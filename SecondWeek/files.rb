7c83c3507aebc2d3b2d25de5945b34217351321f
Brandon Espinoza <espinozab100@gmail.com>
2017-02-08 09:42:26 -0600

    I am able to copy content from a file and paste it to a new or an exisiting file

--- SecondWeek/files.rb
+++ SecondWeek/files.rb
@@ -4,6 +4,8 @@
   # filename = gets.chomp
   # puts open(filename).read
 
+
+
 # # Writing a file
 #   print "What's the file name?"
 #   filename = gets.chomp
@@ -18,17 +20,34 @@
 #   puts "File has been saved"
 
 
-  # Adding to a file
-    print "What's the file name?"
-    filename = gets.chomp
 
-    file = open(filename, "a")
-    puts "What should I add into the file?"
-    line = gets.chomp
-
-    file.write(line + " ")
-
-    file.close
-    puts "File has been saved"
-
+  # # Adding to a file
+  #   print "What's the file name?"
+  #   filename = gets.chomp
+  #
+  #   file = open(filename, "a")
+  #   puts "What should I add into the file?"
+  #   line = gets.chomp
+  #
+  #   file.write(line + " ")
+  #
+  #   file.close
+  #   puts "File has been saved"
     ####### For more modes google 'Ruby file open modes' #######
+
+
+
+    # Copying a file and adding it to another file.
+
+      print "What's the name of the file you want to copy? "
+      filename = gets.chomp
+
+      file_content = open(filename).read
+
+      puts "Where do you want to copy it to?"
+      new_filename  = gets.chomp
+
+      new_file = open(new_filename, "w")
+      new_file.write(file_content)
+      new_file.close
+      puts "File has been saved"
